1452d2180d1a53010b180c3a4f675271f7d961a3
Myron Turner <turnermm02@shaw.ca>
2010-12-05 12:26:34 -0600

    Utility functions which accept the Dokuwiki fnencode value and return results based on that value.

---
+++ SafeFN_util.js
@@ -0,0 +1,37 @@
+/*
+    These two functions are utility functions for  SafeFN_class.js.
+    They accept an encoding parameter, which is the fnencode value
+    set in the Dokuwiki configuration manager.  The will encode/decode
+    based on that parameter.
+   
+*/
+
+function dwikiUTF8_encodeFN(file, encoding){
+
+    if(encoding == 'utf-8') return file;
+
+    if(file.match(/^[a-zA-Z0-9\/_\-\.%]+$/)){
+        return file;
+    }
+
+    if(encoding == 'safe'){
+        return SafeFN_encode(file);
+    }
+
+    file =  encodeURIComponent(file);
+    file =  file.replace(/%2F/g,'/');
+    return file;
+}
+
+
+function dwikiUTF8_decodeFN(file, encoding){
+
+    if(encoding == 'utf-8') return file;
+
+    if(encoding == 'safe'){
+        return SafeFN_decode(file);
+    }
+
+    return decodeURIComponent(file);
+}
+
