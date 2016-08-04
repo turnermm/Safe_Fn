751872809ac836dc5f592b46d64235fe23f6e7f9
Myron Turner <turnermm02@shaw.ca>
2016-08-04 11:04:52 -0500

    added facility in safeconv.php to convert from safe to utf8

--- exe/safeconv.php
+++ exe/safeconv.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+ 
 <?php
 if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/../../');
 require_once DOKU_INC . "inc/init.php";
@@ -8,7 +14,7 @@ if(empty($_SERVER['REMOTE_USER'])) {
 }
 else $client = $_SERVER['REMOTE_USER']; 
 
-echo '<pre>';
+
 global $USERINFO;
  $groups = $USERINFO['grps'];
 if($client && !in_array('admin',$groups)) {
@@ -18,12 +24,18 @@ if($client && !in_array('admin',$groups)) {
 
 if($client && isset($_REQUEST['cmd'])){ 
 
-  recode($conf['datadir']);   
+  $converted = recode($conf['datadir']);   
+  echo  "<h3>$converted files/directories converted</h3>";
 }
 
  function recode($dir) {
       static $safe__test = false; 
       static $x=0;
+      static $type="";
+      if(!$type) {          
+          $type = strtolower($_REQUEST['cmd'][0]);
+          $type= str_replace('-',"",$type);           
+      }
       if(!$safe__test) {        
        $safe__test = new SafeFN;
       }   
@@ -34,33 +46,49 @@ if($client && isset($_REQUEST['cmd'])){
             if(is_dir("$dir/$file")) recode("$dir/$file"); #recurse
             $enc =mb_detect_encoding("$dir/$file", 'auto');  
          
-           if($enc !='UTF-8') { continue;}        
-           
+           if($type == 'safe' && $enc !='UTF-8') {                  
+               continue;
+            }                   
+            else if($type == 'utf8') {    
+                if(strpos("$dir/$file", '%') === false) continue;  
+                if(strpos("$dir/$file", ']') === false) continue;                     
+            }    
           
            $writable= is_writable ("$dir/$file") ? 'writable':   '<span style = "font-weight:bold; font-size:125%;color: red">not writable</span>';
-           echo "$dir/$file ($enc) $writable\n";
+           echo "$dir/$file ($enc) $writable<br />";
             if(!is_writable ("$dir/$file")) continue;           
+           if($type == 'safe')  {          
            $new = cleanID($file);
+           }
+           else $new = $file;
            if(is_dir("$dir/$file")) echo "Directory: ";
+           if($type == 'safe')  {         
            $new = $safe__test->encode($new);
-           echo "Safe: $new\n";                      
-          
-           if(rename("$dir/$file","$dir/$new"))echo "done\n";                     # rename it
+               echo "Safe: $new<br />";                      
+           }
+          else {
+                $new = $safe__test->decode($new);               
+                echo "UTF-8: $new<br />";                      
+          }
+          if(rename("$dir/$file","$dir/$new")) {  $x++; }                     # rename it
           
          echo "\n";
         }
         closedir($dh);
+        return $x;
     }
 
 ?>    
 
 
-</pre>
-<p>This script will convert UTF-8 directory and filenames to Dokuwiki's safe encoding.  It must be run from <dokuwiki>/lib/exe by a user with admin privileges.
+
+<p>This script will convert UTF-8 directory and filenames to Dokuwiki's safe encoding or safe encoding to UTF-8.<br />
+ It must be run from <dokuwiki>/lib/exe by a user logged in with admin privileges.<br />
 The URL: <?php echo htmlspecialchars('http://<your_domain>/<dokuwiki>/lib/exe/safeconv.php');?>
 </p>
    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-          <input type="submit" name="cmd"  value="Convert UTF-8 -> Safe" />   
+           Convert UTF-8 to Dokuwiki Safe Endcoing:  <input type="submit" name="cmd[]"  value="Safe" /> <br />  
+          Convert Dokuwiki Safe Encoding to UTF-8:   <input type="submit" name="cmd[]"  value="UTF-8" />   
     </form>
 
 
