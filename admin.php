9a7c6bcc0879d0ae9027c02feb3f4d2b976818c6
Myron Turner <turnermm02@shaw.ca>
2016-08-06 05:51:42 -0500

    added admin panel for conversions

---
+++ admin.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Plugin SafeFNUtils
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Myron Turner <turnermm02@shaw.ca>
+ */
+
+ 
+/**
+ * All DokuWiki plugins to extend the admin function
+ * need to inherit from this class
+ */
+class admin_plugin_safefnutil extends DokuWiki_Admin_Plugin {
+
+    var $type = '';
+    var $go=false;
+  
+    /**
+     * handle user request
+     */
+    function handle() {
+    
+      if (!isset($_REQUEST['cmd'])) return;   // first time - nothing to do
+
+      $this->type = 'invalid';
+      
+      if (!checkSecurityToken()) return;
+      if (!is_array($_REQUEST['cmd'])) return;
+      $this->go=true;
+      $this->type = key($_REQUEST['cmd']);
+   
+    }
+ 
+    /**
+     * output appropriate html
+     */
+    function html() {
+        
+   
+      ptln("<p>" . $this->getLang('about')  . "<br />");
+   
+
+      ptln('<form action="'.wl($ID).'" method="post">');
+      
+      // output hidden values to ensure dokuwiki will return back to this plugin
+      ptln('  <input type="hidden" name="do"   value="admin" />');
+      ptln('  <input type="hidden" name="page" value="'.$this->getPluginName().'" />');
+      formSecurityToken();
+      ptln('<p style = "line-height:175%">');
+      ptln('Convert UTF-8 to Dokuwiki Safe Endcoing:  <input type="submit" name="cmd[safe]"  value="'.$this->getLang('btn_safe').'" /><br />');  
+      ptln('Convert Dokuwiki Safe Encoding to UTF-8:   <input type="submit" name="cmd[utf8]"   value="'.$this->getLang('btn_utf8').'" /> ');  
+      ptln('</p></form>');
+      
+        if($this->go) ptln('<p> Converting to: '.htmlspecialchars($this->type).'<br /><span id="safeconvutil"></span></p>');
+  
+       ptln('<DIV style = "height:250px;overflow:scroll">');
+       if($this->go)  $this->print_results();
+       ptln('</DIV>');
+    }
+    function print_results() {
+        global $conf;
+        $n = $this->recode($conf['datadir']);  
+         echo "<script type='text/javascript'>\n//<![CDATA[ \n";
+            echo "var n = $n;";
+            echo "var inner=document.getElementById('safeconvutil');"; 
+            echo "inner.innerHTML='Converted $n items'";
+          echo "\n //]]> </script>\n";
+    }
+    function recode($dir) {
+        static $safe__test = false; 
+        static $x=0;
+        static $type;
+       if(!$type) {    
+         $type = $this->type;     
+    
+      }
+      
+      if(!$safe__test) {        
+          $safe__test = new SafeFN;
+      }   
+  
+        $dh = opendir($dir);
+        if(!$dh) return;
+        while (($file = readdir($dh)) !== false) {
+            if($file == '.' || $file == '..') continue;          
+            if(is_dir("$dir/$file")) $this->recode("$dir/$file"); #recurse
+            $enc =mb_detect_encoding("$dir/$file", 'auto');  
+         
+           if($type == 'safe' && $enc !='UTF-8') {                  
+               continue;
+            }                   
+            else if($type == 'utf8') {    
+                if(strpos("$dir/$file", '%') === false) continue;  
+                if(strpos("$dir/$file", ']') === false) continue;                     
+            }    
+                             
+           $writable= is_writable ("$dir/$file") ? 'writable':   '<span style = "font-weight:bold; font-size:125%;color: red">not writable</span>';
+           echo "$dir/$file ($enc) $writable<br />";
+            if(!is_writable ("$dir/$file")) continue;   
+           if($type == 'safe')  {          
+               $new = cleanID($file);
+           }
+           else $new = $file;
+           if(is_dir("$dir/$file")) echo "Directory: ";
+           if($type == 'safe')  {         
+               $new = $safe__test->encode($new);               
+               echo "Safe: $new<br />";                      
+           }
+          else {
+                $new = $safe__test->decode($new);               
+                echo "UTF-8: $new<br />";                      
+          }
+          if(rename("$dir/$file","$dir/$new")) {  $x++; }                     # rename it
+          
+         echo "\n";
+        }
+        closedir($dh);
+        return $x;
+    }
+    
+}
---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christopher Smith <chris@jalakai.co.uk>
+ */
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'SafeFNUtils Converter' ; 
+$lang['btn_safe'] = 'Safe';
+$lang['btn_utf8'] = 'UTF-8';
+$lang['about'] ="This app will convert UTF-8 directory and filenames to Dokuwiki's safe encoding or safe encoding to UTF-8";
