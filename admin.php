7e15168dc407b294e4d27efb8fda5c3d13e50e05
Myron Turner <turnermm02@shaw.ca>
2016-08-23 16:11:28 -0500

    updates to admin.php

--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@ class admin_plugin_safefnutil extends DokuWiki_Admin_Plugin {
      * output appropriate html
      */
     function html() {
-        
+      global $conf;  
    
       ptln("<p>" . $this->getLang('about')  . "<br />");
    
@@ -51,23 +51,85 @@ class admin_plugin_safefnutil extends DokuWiki_Admin_Plugin {
       ptln('Convert UTF-8 to Dokuwiki Safe Endcoing:  <input type="submit" name="cmd[safe]"  value="'.$this->getLang('btn_safe').'" /><br />');  
       ptln('Convert Dokuwiki Safe Encoding to UTF-8:   <input type="submit" name="cmd[utf8]"   value="'.$this->getLang('btn_utf8').'" /> ');  
       ptln('</p></form>');
-      
-        if($this->go) ptln('<p> Converting to: '.htmlspecialchars($this->type).'<br /><span id="safeconvutil"></span></p>');
+      ptln('Current encoding: ' .  $conf['fnencode'] ) ;
+      if($this->go) ptln('<p> Converting to: '.htmlspecialchars($this->type).'<br /><span id="safeconvutil"></span></p>');
   
        ptln('<DIV style = "height:250px;overflow:scroll">');
        if($this->go)  $this->print_results();
        ptln('</DIV>');
     }
     function print_results() {
+        
+        
         global $conf;
-        $n = $this->recode($conf['datadir']);  
-         echo "<script type='text/javascript'>\n//<![CDATA[ \n";
-            echo "var n = $n;";
+        echo "<b>" . $conf['datadir'] . "</b><br>";
+        $files = $this->recode($conf['datadir']);  
+        echo "<br /><b>" . $conf['mediadir'] . "</b><br />";
+        $media =  $this->recode($conf['mediadir'],true);         
+        
+         echo "<script type='text/javascript'>\n//<![CDATA[ \n"; 
             echo "var inner=document.getElementById('safeconvutil');"; 
-            echo "inner.innerHTML='Converted $n items'";
+            echo "inner.innerHTML='Converted $files  files , $media media'";
           echo "\n //]]> </script>\n";
     }
-    function recode($dir) {
+    
+    function recode($dir, $nmedia = false) {
+        static $safe__test = false; 
+        static $x=0;
+        if($nmedia) $x=0;
+        static $type;
+       if(!$type) {    
+         $type = $this->type;     
+    
+      }
+      
+      if(!$safe__test) {        
+          $safe__test = new SafeFN;
+      }   
+  
+        $dh = opendir($dir);
+        if(!$dh) return;
+        while (($file = readdir($dh)) !== false) {
+            if($file == '.' || $file == '..') continue;          
+            if(is_dir("$dir/$file")) $this->recode("$dir/$file"); #recurse
+            $enc =mb_detect_encoding("$dir/$file", 'auto');  
+         
+           if($type == 'safe' && $enc !='UTF-8') {                  
+               continue;
+            }                   
+            else if($type == 'utf8') {    
+           
+                if(strpos("$dir/$file", '%') === false) continue;  
+                if(strpos("$dir/$file", ']') === false) continue;  
+                if(!$safe__test->validate_safe("$dir/$file")) continue;
+            }    
+                             
+           $writable= is_writable ("$dir/$file") ? 'writable':   '<span style = "font-weight:bold; font-size:125%;color: red">not writable</span>';
+           echo "$dir/$file ($enc) $writable<br />";
+            if(!is_writable ("$dir/$file")) continue;   
+           if($type == 'safe')  {          
+               $new = cleanID($file);
+           }
+           else $new = $file;
+           if(is_dir("$dir/$file")) echo "Directory: ";
+           if($type == 'safe')  {         
+               $new = $safe__test->encode($new);               
+               echo "Safe: $new<br />";                      
+           }
+          else {
+                $new = $safe__test->decode($new);               
+                echo "UTF-8: $new<br />";                      
+          }
+         if(rename("$dir/$file","$dir/$new")) {  $x++; }                     # rename it
+          
+         echo "\n";
+        }
+        closedir($dh);
+        return $x;
+    }
+/*  ------------  */
+
+    function media_recode($dir) {
         static $safe__test = false; 
         static $x=0;
         static $type;
@@ -111,12 +173,14 @@ class admin_plugin_safefnutil extends DokuWiki_Admin_Plugin {
                 $new = $safe__test->decode($new);               
                 echo "UTF-8: $new<br />";                      
           }
-          if(rename("$dir/$file","$dir/$new")) {  $x++; }                     # rename it
+    //      if(rename("$dir/$file","$dir/$new")) {  $x++; }                     # rename it
           
          echo "\n";
         }
         closedir($dh);
         return $x;
     }
+
+
     
 }
